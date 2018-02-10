dbafc395cf4d9261c942797bf2c3829889db74e1
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-02-09 22:47:52 -0500

    flag elucidated

--- flag.go
+++ flag.go
@@ -7,7 +7,11 @@ import (
 
 func main() {
 	flagName := flag.String("name", "default", "description")
+	flagBool := flag.Bool("bool", false, "boolean")
+	flagInt := flag.Int("int", 0, "integer")
 	flag.Parse()
 	fmt.Println(*flagName)
-	// Usage: go run main.go -name=whatever
+	fmt.Println(*flagBool)
+	fmt.Println(*flagInt)
+	// Usage: go run main.go -name=whatever -bool=true -int=81
 }
